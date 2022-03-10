72afbbb60114b25f47b4c4b2dda26df11261c542
tam <vuthitam14082001@gmail.com>
2022-03-10 12:01:03 +0700

    edit css header, banner, courses

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,7 +1,7 @@
 const menuList = document.querySelector("#header-menu-list");
-const button = document.getElementById ("toggle");
+const button = document.getElementById("show-menu");
 
-button.onclick = function(){
+button.onclick = function () {
   menuList.classList.toggle("show-menu");
   document.querySelector(".fa-solid").classList.toggle("fa-xmark");
 }
