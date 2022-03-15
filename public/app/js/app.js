3f5cd69a99efe175d7eedb0b6285b0317cb38297
tam <vuthitam14082001@gmail.com>
2022-03-15 08:00:33 +0700

    edit css feedback, footer

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
-  $("#showMenu").click(function(){
-    $("#headerMenuList").toggleClass("show-menu");
-    $(".fa-solid").toggleClass("fa-xmark");
+  $('#showMenu').click(function () {
+    $('#headerMenuList').toggleClass('show-menu');
+    $('.fa-solid').toggleClass('fa-xmark');
   })
 
   $('.slick-slider').slick({
     slidesToShow: 2,
     slidesToScroll: 2,
-    prevArrow: "<button class='slick-prev slick-arrow'><i class='fa-solid fa-angle-left'></i></button>",
-    nextArrow: "<button class='slick-next slick-arrow'><i class='fa-solid fa-angle-right'></i></button>",
+    prevArrow: '<button class="slick-prev slick-arrow"><i class="fa-solid fa-angle-left"></i></button>',
+    nextArrow: '<button class="slick-next slick-arrow"><i class="fa-solid fa-angle-right"></i></button>',
 
     responsive: [
       {
@@ -21,29 +21,34 @@ $(document).ready(function () {
     ]
   });
 
-  $("#loginRegisterFormBtn").click(function(){
-    $("#loginRegisterForm").addClass("active-form");
+  $('#loginRegisterFormBtn').click(function () {
+    $('#loginRegisterForm').addClass('active-form');
   })
 
-  $("#closeForm").click(function(){
-    $("#loginRegisterForm").removeClass("active-form");
+  $('#closeForm').click(function () {
+    $('#loginRegisterForm').removeClass('active-form');
   })
 
-  $("#openLoginFormBtn").click(function(){
-    $("#loginForm").css("display", "block");
-    $("#registerForm").css("display", "none");
+  $('#openLoginFormBtn').click(function () {
+    $('#loginForm').css('display', 'block');
+    $('#registerForm').css('display', 'none');
   })
 
-  $("#openRegisterFormBtn").click(function(){
-    $("#loginForm").css("display", "none");
-    $("#registerForm").css("display", "block");
+  $('#openRegisterFormBtn').click(function () {
+    $('#loginForm').css('display', 'none');
+    $('#registerForm').css('display', 'block');
   })
 
-  $("#openMessageBtn").click(function(){
-    $("#message").toggleClass("open-message");
+  $('#openMessageBtn').click(function () {
+    $('#message').toggleClass('open-message');
   })
 
-  $("#closeMessageBtn").click(function(){
-    $("#message").removeClass("open-message");
+  $('#closeMessageBtn').click(function () {
+    $('#message').removeClass('open-message');
+  })
+
+  $('.contact-logo').mouseenter(function () {
+    $('.contact-logo > a').toggleClass('contact-active');
+    $('.info-contact').toggleClass('info-contact-active');
   })
 });
