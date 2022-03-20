002ef75ecf67b2c909be1c7abdf6cea434087fae
tam <vuthitam14082001@gmail.com>
2022-03-20 23:23:42 +0700

    home page

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,7 +1,55 @@
-const menuList = document.getElementById("headerMenuList");
-const button = document.getElementById("showMenu");
+$(document).ready(function () {  
+  $('#showMenu').click(function () {
+    $('#headerMenuList').toggleClass('show-menu');
+    $('#showMenu > i').toggleClass('fa-xmark');
+  })
 
-button.onclick = function () {
-  menuList.classList.toggle("show-menu");
-  document.querySelector(".fa-solid").classList.toggle("fa-xmark");
-}
+  $('.slick-slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    prevArrow: '<button class="slick-prev slick-arrow"><i class="fa-solid fa-angle-left"></i></button>',
+    nextArrow: '<button class="slick-next slick-arrow"><i class="fa-solid fa-angle-right"></i></button>',
+
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  $('#loginRegisterFormBtn').click(function () {
+    $('#loginRegisterForm').addClass('active-form');
+  })
+
+  $('#closeForm').click(function () {
+    $('#loginRegisterForm').removeClass('active-form');
+  })
+
+  $('#openLoginFormBtn').click(function () {
+    $('#loginForm').css('display', 'block');
+    $('#registerForm').css('display', 'none');
+    $(this).addClass('active');
+    $('#openRegisterFormBtn').removeClass('active');
+  })
+
+  $('#openRegisterFormBtn').click(function () {
+    $('#loginForm').css('display', 'none');
+    $('#registerForm').css('display', 'block');
+    $(this).addClass('active');
+    $('#openLoginFormBtn').removeClass('active');
+  })
+
+  $('#openMessageBtn').click(function () {
+    $('#message').toggleClass('open-message');
+  })
+
+  $('#closeMessageBtn').click(function () {
+    $('#message').removeClass('open-message');
+  })
+
+  $('[data-toggle="tooltip"]').tooltip();
+});
