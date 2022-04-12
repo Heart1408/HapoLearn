dfcb7e15f6bd0d3e75d8581e1d8cb818577f4fa5
tam <vuthitam14082001@gmail.com>
2022-04-12 12:42:49 +0700

    create login register

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Course;
+use App\Models\Lesson;
+use App\Models\User;
+use App\Models\Review;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -13,7 +18,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
     }
 
     /**
@@ -23,6 +28,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $courses = DB::table('courses')->select('name', 'description', 'logo')->limit(3)->get();
+        $sumcourse = DB::table('courses')->count();
+        $sumlesson = DB::table('lessons')->count();
+        $learners = DB::table('users')->where('role', '=', 1)->count();
+        $reviews = DB:: table('reviews')
+            ->join('users', 'reviews.user_id', '=', 'users.id')
+            ->select('reviews.comment', 'reviews.rate', 'users.name', 'users.avartar')->limit(4)->get();
+        // dd($reviews);
+        return view('homepage', compact('courses', 'sumcourse', 'sumlesson', 'learners', 'reviews'));
     }
 }
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,55 @@
+<section class="header">
+    @if (session('success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{session('success')}}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @elseif (session('error'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Error: </strong> {{session('error')}}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+    <nav>
+        <div class="header-menu">
+            <div class="header-logo">
+                <div class="logo-img">
+                    <img src="{{ asset('images/hapo_learn_logo.png') }}" alt="HapoLearn logo">
+                </div> 
+            </div>
+            <ul class="header-menu-list" id="headerMenuList">
+                <li>
+                    <a href="#">HOME</a>
+                </li>
+                <li>
+                    <a class="active-menu" href="#">ALL COURSES</a>
+                </li>
+                @if (@Auth::check()) 
+                    <li class="dropdown row">
+                        <a class="dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false">
+                            {{ Auth()->user()->name }}
+                        </a>
+                        <div class="img">
+                            <img src="{{ asset(Auth()->user()->avartar) }}" alt="avartar">
+                        </div>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Profile</a>
+                            <a class="dropdown-item" href="{{ route('logout') }}">Logout</a>
+                        </div>
+                    </li>
+                @else
+                    <li>
+                        <a id="loginRegisterFormBtn" href="#">LOGIN/REGISTER</a>
+                    </li>
+                @endif
+            </ul>
+            <button class="toggle" id="showMenu">
+                <i class="fa-solid fa-bars"></i>
+            </button>
+        </div>
+    </nav>
+</section>
